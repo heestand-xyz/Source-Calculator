b74ace680f85fb03192053044eafada6303030c0
Hexagons <anton.heestand@gmail.com>
2019-12-04 14:41:59 +0100

    Initial Commit

---
+++ Source Calculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Source Calculator
+//
+//  Created by Anton Heestand on 2019-12-04.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
